0e408d6c9b9fcfe2e1dc9d6027d44c49f47794a6
mrzaizai2k <chibao24.12.1999@gmail.com>
2025-01-02 11:12:35 +0700

    create dataset for finetuning

--- src/Utils/utils.py
+++ src/Utils/utils.py
@@ -11,10 +11,43 @@ import pytesseract
 from collections import Counter
 import cv2
 import numpy as np
+import requests
+import warnings
+import contextlib
+from urllib3.exceptions import InsecureRequestWarning
 
 
 from dotenv import load_dotenv
 load_dotenv()
+
+
+# SSL Verification Context Manager
+@contextlib.contextmanager
+def no_ssl_verification():
+    old_merge_environment_settings = requests.Session.merge_environment_settings
+    opened_adapters = set()
+
+    def merge_environment_settings(self, url, proxies, stream, verify, cert):
+        opened_adapters.add(self.get_adapter(url))
+        settings = old_merge_environment_settings(self, url, proxies, stream, verify, cert)
+        settings['verify'] = False
+        return settings
+
+    requests.Session.merge_environment_settings = merge_environment_settings
+
+    try:
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore', InsecureRequestWarning)
+            yield
+    finally:
+        requests.Session.merge_environment_settings = old_merge_environment_settings
+
+        for adapter in opened_adapters:
+            try:
+                adapter.close()
+            except:
+                pass
+
 
 
 def convert_ms_to_hms(ms):
